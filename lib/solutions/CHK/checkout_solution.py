685e62bf3e7768f5255b645bd65f791b97b09554
sbx_user1051 <sbx_user1051@169.254.238.73>
2023-10-23 21:11:22 +0000

    Mon Oct 23 21:11:22 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,5 +1,7 @@
 
 
+from collections import Counter
+
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
@@ -7,10 +9,19 @@ def checkout(skus):
     if(not isinstance(skus, str)) :
         return -1
     else :
+        print(skus)
+        return 2
         # define special offers (not yet)
         # split string into list & count occurrences of each character
-        skus = list(skus)
-        # return sum
+        # skus = list(skus)
+        # basket = Counter(skus)
+
+        # # return sum
+        # basketSum = 0
+        # for item, count in basket.items():
+        #     basket +=
+        
     
+
 
 
