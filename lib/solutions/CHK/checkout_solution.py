640baf2e3134f16a27547868f4a2486e9b7f570c
sbx_user1051 <sbx_user1051@169.254.238.73>
2023-10-23 21:05:22 +0000

    Mon Oct 23 21:05:22 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,4 +3,11 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+    # verify input
+    if(not isinstance(skus, str)) :
+        return -1
+    else :
+        # split string into list & count occurrences of each 
+        # 
+    
+
