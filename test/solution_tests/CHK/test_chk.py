c110ab08178e7649457e4a5b4500c01f0531fe77
sbx_user1051 <sbx_user1051@169.254.238.73>
2023-10-23 21:48:06 +0000

    Mon Oct 23 21:48:06 UTC 2023

--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -1,1 +1,8 @@
-from challenges import 
+from lib.solutions.CHK.checkout_solution import checkout
+
+testData = {
+    { 'skus' : 'AAABB', 'answer' : 175 }
+}
+
+for test in testData:
+    assert test['answer'] == checkout(test['skus'])
--- test/solution_tests/TST/test_one.py
+++ test/solution_tests/TST/test_one.py
@@ -1,6 +1,9 @@
+import sys
+sys.path.append(1, '')
 from solutions.TST import one
 
 
 class TestSum():
     def test_sum(self):
         assert one.get() == 1
+
