4bf135da27a16f48aa1bfa7d3b53408356af051f
sbx_user1051 <sbx_user1051@169.254.238.73>
2023-10-23 20:51:50 +0000

    Mon Oct 23 20:51:50 UTC 2023

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -3,4 +3,7 @@
 # noinspection PyUnusedLocal
 # friend_name = unicode string
 def hello(friend_name):
+    if(not isinstance(friend_name, str)) : friend_name = str(friend_name)
+    
     raise NotImplementedError()
+
