835959988c96c77c98de09bef9ce58908353c409
sbx_user1051 <sbx_user1051@169.254.102.57>
2023-10-25 17:22:53 +0000

    Wed Oct 25 17:22:53 UTC 2023

--- lib/solutions/CHK/test_checkout.py
+++ lib/solutions/CHK/test_checkout.py
@@ -2,11 +2,20 @@
 from checkout_solution import checkout
 
 testData = {
-    { "basket" : "ABCDE", "expectedSum" : }
+    { "basket" : "",      "expectedSum" : -1 },
+    { "basket" : "A",     "expectedSum" : 50 },
+    { "basket" : "B",     "expectedSum" : 30 },
+    { "basket" : "C",     "expectedSum" : 20 },
+    { "basket" : "D",     "expectedSum" : 15 },
+    { "basket" : "E",     "expectedSum" : 40 },
+    { "basket" : "ABCDE", "expectedSum" : 155 },
 
 }
 def test_checkout():
-    print(checkout('ABCDE'))
+    for test in testData:
+        assert checkout(test['basket']) == test["expectedSum"]
+    
+    print("Passed")
 
 
 test_checkout()
